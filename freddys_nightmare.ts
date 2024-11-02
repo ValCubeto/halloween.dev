52d8701bac9618c3150c34b8f87e7f26018ccb61
ValCubeta <valcubetaa@gmail.com>
2024-11-02 20:01:54 -0300

    asdadasdas

--- freddys_nightmare.ts
+++ freddys_nightmare.ts
@@ -1,4 +1,9 @@
 function findSafestPath(dream: number[][]): number {
+  let danger = 0;
+  for (let i = 1; i < dream[0].length; i += 1) {
+    let path = dream[0].slice(0, i);
+    for (let j = )
+  }
   return 0;
 }
 
